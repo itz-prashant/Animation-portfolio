6d7bf714e9b9f11612bba9e66219c2a63b0db4cb
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-02-13 13:45:18 +0530

    Add downoad cv button

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Sidebar from "./components/Sidebar"
 function App() {
 
   return (
-    <div className="bg-[url(./assets/background.png)] object-cover bg-[50%] bg-blend-soft-light bg-dark w-full h-screen overflow-hidden z-0 font-display">
-      <div className="max-w-[900px] mx-auto flex">
+    <div className="bg-[url(./assets/background.png)] object-cover bg-[50%] bg-blend-soft-light bg-dark w-full h-screen  z-0 font-display">
+      <div className="max-w-[1100px] mx-auto flex">
         <Sidebar />
         <Main />
       </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,10 +7,12 @@ import {
   FaInstagramSquare,
   FaFacebookSquare, FaRegAddressCard, FaBriefcase  
 } from "react-icons/fa";
+import { RiFileDownloadLine } from "react-icons/ri";
+
 
 const Sidebar = () => {
   return (
-    <div className="max-w-[249px] w-full h-screen bg-light-bg flex flex-col justify-between">
+    <div className="max-w-[249px] w-full h-[100vh]  bg-light-bg flex flex-col justify-between overflow-y-scroll [&::-webkit-scrollbar]:w-2">
       <div className="w-full flex flex-col items-center gap-2 justify-between mt-5">
         <div className="w-[210px]">
           <img src={sidebarImg} alt="" className="w-[210px] " />
@@ -20,19 +22,19 @@ const Sidebar = () => {
         </h1>
         <p className="text-light">Developer</p>
         <div className="flex items-center gap-3">
-          <span className="w-12 h-12 rounded-full flex items-center justify-center bg-dark-bg hover:bg-pink cursor-pointer duration-500 text-white text-xl p-1">
+          <span className="w-12 h-12 rounded-full flex items-center justify-center bg-dark-bg hover:bg-pink cursor-pointer duration-500 text-white text-2xl">
             <FaLinkedin />
           </span>
-          <span className="w-12 h-12 rounded-full flex items-center justify-center bg-dark-bg hover:bg-pink cursor-pointer duration-500 text-white text-xl p-1">
+          <span className="w-12 h-12 rounded-full flex items-center justify-center bg-dark-bg hover:bg-pink cursor-pointer duration-500 text-white text-2xl">
             <FaFacebookSquare />
           </span>
-          <span className="w-12 h-12 rounded-full flex items-center justify-center bg-dark-bg hover:bg-pink cursor-pointer duration-500 text-white text-xl p-1">
+          <span className="w-12 h-12 rounded-full flex items-center justify-center bg-dark-bg hover:bg-pink cursor-pointer duration-500 text-white text-2xl">
             <FaInstagramSquare />
           </span>
         </div>
       </div>
 
-      <div>
+      <div className="mt-8">
         <ul className="flex flex-col text-white">
           <li className="flex gap-2 items-center border-t border-light p-3">
             <span className="text-2xl"><IoMdHome /></span>
@@ -57,7 +59,14 @@ const Sidebar = () => {
         </ul>
       </div>
 
-      <div></div>
+      <div className="h-[120px] py-3 px-2">
+        <div className="w-full bg-dark-bg flex rounded-full items-center relative h-[60px] px-3 hover:bg-pink duration-1000 group">
+          <div className="absolute w-12 h-12 rounded-full flex items-center justify-center bg-pink group-hover:bg-white group-hover:text-pink cursor-pointer text-white text-3xl left-3 group-hover:right-3 duration-500 ">
+              <RiFileDownloadLine />
+          </div>
+          <p className="text-white right-3 group-hover:left-3  absolute">Download CV</p>
+        </div>
+      </div>
     </div>
   );
 };
