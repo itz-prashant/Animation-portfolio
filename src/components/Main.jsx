9034aafa7d5a33cb6af8b3b20cbd4fbe4ccc2fe7
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-02-16 15:52:40 +0530

    Add About us page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,4 @@
+import AboutMe from "../pages/AboutMe"
 import Home from "../pages/Home"
 
 const Main = () => {
@@ -5,6 +6,7 @@ const Main = () => {
     <div className=" w-full h-screen lg:w-[850px] md:w-2xl  sm:w-[500px] mx-auto float-right bg-dark-bg overflow-y-scroll [&::-webkit-scrollbar]:w-1 [&::-webkit-scrollbar-thumb]:bg-green lg:ml-[250px] z-20">
       <div className="mt-5 px-5">
           <Home />
+          <AboutMe />
       </div>
     </div>
   )
