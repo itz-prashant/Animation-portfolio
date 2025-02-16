57fe1cce257e2dff0c1b5c28c5fd46757a3f73d2
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-02-16 19:01:45 +0530

    Fix margin and font size

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 
 const Button = ({text, color}) => {
   return (
-    <button className={`text-dark py-3 px-5 min-w-[150px] w-fit flex justify-center items-center text-xl ${color == "green"? "bg-green": "bg-yellow"} rounded-full relative overflow-hidden group cursor-pointer`}>
+    <button className={`text-dark py-3 px-5 min-w-[150px] w-fit flex justify-center items-center  ${color == "green"? "bg-green": "bg-yellow"} ${text == "View Work" || text ==  "Contact" ? "xsm:text-xl text-base": "text-xl"} rounded-full relative overflow-hidden group cursor-pointer`}>
         <p className="z-[100] group-hover:text-white duration-500">{text}</p>
         <div className="absolute right-0 left-0 -top-15 bg-pink h-13 group-hover:top-0 duration-500"></div>
     </button>
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,57 +1,57 @@
 
 const AboutMe = () => {
   return (
-    <div className="mt-10 text-white flex flex-col gap-8">
+    <div className="mt-20 text-white flex flex-col gap-8">
         <div className="flex items-baseline gap-4 justify-between">
             <h1 className="sm:text-5xl text-4xl w-fit shrink-0 font-bold">About Me.</h1>
             <div className="h-[1.6px] w-full bg-white/30 "></div>
         </div>
         <p className="sm:text-xl text-lg text-light">Hi, my name is <span className="text-white font-semibold">Ruchita Sharma</span> and I began using WordPress when first began. I've spent most of my waking hours for the last ten years designing, programming and operating WordPress sites go beyond with exclusive designer. Apart from this I love to travel, mentor designers, review design portfolios & read books on everything related to design. I have also given design talks in various educational institutions. So I love creating creative content, and you can find most of my works here.</p>
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-10">
             <div className="flex text-lg">
-                <span className="text-light w-[130px] shrink">Name</span>
+                <span className="text-light w-[100px] xsm:w-[130px] shrink">Name</span>
                 <div className="flex gap-3">
                     <span className="text-light">:</span>
                     <span className="font-semibold">Ruchita Sharma</span>
                 </div>
             </div>
             <div className="flex text-lg">
-                <span className="text-light w-[130px] shrink">Nationality</span>
+                <span className="text-light w-[100px] xsm:w-[130px] shrink">Nationality</span>
                 <div className="flex gap-3">
                     <span className="text-light">:</span>
                     <span className="font-semibold">Indian</span>
                 </div>
             </div>
             <div className="flex text-lg">
-                <span className="text-light w-[130px] shrink">Phonw</span>
+                <span className="text-light w-[100px] xsm:w-[130px] shrink">Phonw</span>
                 <div className="flex gap-3">
                     <span className="text-light">:</span>
                     <span className="font-semibold">+91 XXXXXXXXXX</span>
                 </div>
             </div>
             <div className="flex text-lg">
-                <span className="text-light w-[130px] shrink">Email</span>
+                <span className="text-light w-[100px] xsm:w-[130px] shrink">Email</span>
                 <div className="flex gap-3">
                     <span className="text-light">:</span>
                     <span className="font-semibold">hello@gmail.com</span>
                 </div>
             </div>
             <div className="flex text-lg">
-                <span className="text-light w-[130px] shrink">Experience</span>
+                <span className="text-light w-[100px] xsm:w-[130px] shrink">Experience</span>
                 <div className="flex gap-3">
                     <span className="text-light">:</span>
                     <span className="font-semibold">+2 Year</span>
                 </div>
             </div>
             <div className="flex text-lg">
-                <span className="text-light w-[130px] shrink">Skype</span>
+                <span className="text-light w-[100px] xsm:w-[130px] shrink">Skype</span>
                 <div className="flex gap-3">
                     <span className="text-light">:</span>
                     <span className="font-semibold">hello@examole</span>
                 </div>
             </div>
             <div className="flex text-lg">
-                <span className="text-light w-[130px] shrink">Language</span>
+                <span className="text-light w-[100px] xsm:w-[130px] shrink">Language</span>
                 <div className="flex gap-3">
                     <span className="text-light">:</span>
                     <span className="font-semibold">English / Hindi</span>
